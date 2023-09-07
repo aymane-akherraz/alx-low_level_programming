a0195b14e47953dd60650db5bad773b65ba55386
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-08 00:39:48 +0100

    Modified some files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof(char),
+	strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),
+	stderr);
 	return (1);
 }
