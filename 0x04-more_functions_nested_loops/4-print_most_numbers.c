a0396c8c349d8cf3ae40239a8d4ae72dc15a25e7
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-14 21:49:46 +0100

    Modified 3-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers, from 0 to 9, except 2 and 4
+ * print_most_numbers - Prints the numbers, from 0 to 9, except 2 and 4
  * followed by a new line.
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
