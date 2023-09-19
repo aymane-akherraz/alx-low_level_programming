6dd95ecbc89f5dc7ba66c4d164f2a9edce43a9c5
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-19 23:45:44 +0100

    Added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer.
+ * @n : The given integer
+*/
+
+void print_number(int n)
+{
+	unsigned int d;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		d = -n;
+	}
+	else
+		d = n;
+
+	if (n != 0)
+	{
+		if (d / 10 != 0)
+			print_number(d / 10);
+
+		_putchar((d % 10) + '0');
+	}
+	else
+		_putchar('0');
+}
