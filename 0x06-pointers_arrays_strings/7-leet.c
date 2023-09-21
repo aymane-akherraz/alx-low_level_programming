1a64e87ce47c5b878dc8405fecf0a216fe8a0f98
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-21 13:15:53 +0100

    Added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+ * leet - Encodes a string into 1337.
+ * @s: the given string
+ * Return: s, the resulted string
+*/
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char l[] = {'a', 'e', 'o', 't', 'l'};
+	char v[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+				s[i] = v[j];
+		}
+	}
+	return (s);
+}
