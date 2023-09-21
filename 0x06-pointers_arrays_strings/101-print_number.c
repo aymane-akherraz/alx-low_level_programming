60ca070c078b2316a408b7e79e0e2b9651cf4ad1
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-21 23:25:09 +0100

    Modified 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,13 +16,8 @@ void print_number(int n)
 	else
 		d = n;
 
-	if (n != 0)
-	{
-		if (d / 10 != 0)
-			print_number(d / 10);
+	if (d / 10 != 0)
+		print_number(d / 10);
 
-		_putchar((d % 10) + '0');
-	}
-	else
-		_putchar('0');
+	_putchar((d % 10) + '0');
 }
