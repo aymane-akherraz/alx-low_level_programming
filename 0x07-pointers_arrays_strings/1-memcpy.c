a4179f3ca6d792978cd0b5d7c8cfe24d3f9e3094
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-26 03:14:53 +0100

    Modified 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
 
-	if (n)
+	for (i = 0; n > 0; i++)
 	{
-		for (i = 0; i < n; i++)
-			dest[i] = src[i];
+		dest[i] = src[i];
+		n--;
 	}
 	return (dest);
 }
