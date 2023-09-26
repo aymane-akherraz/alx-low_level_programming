afa5cac3d271a79d73782ca1a7118000283476bf
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-09-26 04:03:42 +0100

    Modified 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,30 +8,26 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	if (needle[0] == '\0')
-	{
-		return (haystack);
-	}
+	int i, j, k;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j] != '\0'; j++)
+			k = i + 1;
+			for (j = 1; needle[j] != '\0'; j++)
 			{
-				if (haystack[i + j] != needle[j])
+				if (haystack[k] == needle[j])
 				{
-					break;
+					k++;
+					continue;
 				}
+				else
+					break;
 			}
-
 			if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
+				return (&haystack[i]);
 		}
 	}
-	return ('\0');
-}	
+	return (0);
+}
