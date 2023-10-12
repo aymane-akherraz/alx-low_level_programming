bb6524f7d7a2fc4ec934580f51cf4a31dfcfbefb
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-10-12 01:51:12 +0100

    Modified some files

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct op - Struct op
  *
@@ -15,11 +16,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - Selects the correct function to perform
@@ -22,12 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s && s[1] == '\0')
+		if (*(ops[i].op) == *s)
 			return (ops[i].f);
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
+	return (NULL);
 }
