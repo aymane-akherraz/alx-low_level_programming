e9e4d1ba30db66284a948f5f1719a12b65101ed6
Aymane Akherraz <aymaneakherraz@gmail.com>
2023-10-24 23:28:47 +0100

    Modified 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 	ptr = *head;
 	tmp = (*head)->next;
 	
-	if (head == NULL || ptr == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
 	if (tmp == NULL)
